854a8c1ae86702e3576cb64cd4c7a843f9df0b9c
R Azam <ra444@student.le.ac.uk>
2023-02-01 11:41:27 +0000

    Create navbar

---
+++ resources/views/templates/navbar.blade.php
@@ -0,0 +1,26 @@
+<html>
+
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://kit.fontawesome.com/cd1181b8ae.js" crossorigin="anonymous"></script>
+  </head>
+  
+  <body>
+  <header>
+ <div id="collapse">
+ <button class="open" onclick="open()"><i class="fa-solid fa-bars"></i></button>
+ 
+ </div>
+ 
+ 
+ <nav id="navbar" class="Navbar">
+  <a href="javascript;void(0)" class="close()" onclick="close">X</a>
+ <ul>
+   <li><a href="">Dashboard Page</a></li>
+   <li> <a href="">About Us</a> </li>
+ </ul>
+ </nav>
+ </header>
+  </body>
+
+</html>
