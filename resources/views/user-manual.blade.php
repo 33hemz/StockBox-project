b6bf9eef2401ebcb5173630f13f8b1d82cfe2770
S S Cheema <ssc26@student.le.ac.uk>
2023-02-03 17:06:45 +0000

    Create user manual

---
+++ resources/views/user-manual.blade.php
@@ -0,0 +1,46 @@
+@extends('templates.master')
+
+@section('title', 'User Manual')
+
+@section('content') 
+<h1>
+    User Manual
+</h1>
+<h2>
+  The landing page:
+</h2>
+<p>
+    This is the first page you will come across. It will present a brief description and overview of what StockBox is about.
+    You can login from here to view details of your business and marketplace.
+</p>
+<h2>
+    The login page:
+</h2>
+<p>
+    The login page will be where you will enter your valid username & password to access your account.
+</p>
+<h2>
+The dashboard:
+</h2>
+<p>
+    This will show the data and graphs of the data provided for the market.
+</p>
+<h2>
+    Account
+</h2>
+<p>
+    Your account details displayed here.
+</p>
+<h2>
+    Features
+</h2>
+<p>
+    Features of the dashboard presented here
+</p>
+<h2>
+    Support
+</h2>
+<p>
+    Support given here.
+</p>
+@endsection
