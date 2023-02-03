96b16d86b03184955392dfccc5f267cf741ba312
George Rogers <georgerogers@host-49-100.eduroam-local.wifi.le.ac.uk>
2023-02-03 16:03:46 +0000

    Add login page functionality

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function index() {
+        return view("login");
+    }
+
+    public function login() {
+    
+        validator(request()->all(), [
+            'username' => 'required',
+            'password' => 'required'
+        ]) ->validate();
+
+        return redirect('dashboard');
+    }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,4 +6,27 @@
 
 @section('content') 
 <h1>Login Page</h1>
+<form action="/processlogin" method="post">
+    @csrf
+    <label for="username">Username: </label>
+    <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" id="username">
+    
+@error('username')
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+    @enderror
+    <br>
+
+    <label for="password">Password: </label>
+    <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password">
+    @error('password')
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+    @enderror
+    <br>
+
+    <input type="submit" class="btn btn-primary">
+</form>
 @endsection
