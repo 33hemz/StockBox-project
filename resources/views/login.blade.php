3154027a9c2d768561872a932860fdb97320e01e
A Igandan <ai189@student.le.ac.uk>
2023-02-04 06:50:10 +0000

    Minor styling improvements

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,6 +12,10 @@
         background-color: #2db28e;
     }
 
+
+
+
+
 </style>
 @endsection
 
@@ -20,7 +24,7 @@
     <div class="card shadow-lg" style="background-color: aliceblue">
         <img id="logo" src="{{ asset('assets/Full Logo/PNG/StokBox-Square-01.png') }}" width="150" alt="Logo" class="center">
         
-        <h1 class="center">Stokbox Analytics</h1>
+        <h1 class="center" style="font-size: 25px"><b>Stokbox Analytics</b></h1>
         <div style="padding: 50px;">
             <form action="/processlogin" method="post" class="w-full max-w-sm">
                 @csrf
@@ -45,7 +49,7 @@
                 @enderror
                 </div>
                 
-                <div><a style="font-size:11px" href="{{ url('/user-manual') }}">Forgot your Password?</a></div>
+                <div><a style="font-size:11px" href="{{ url('/user-manual') }}"><em>Forgot your Password?</em></a></div>
                 <div>
                     <button type="submit" style="background-color: #2db28e" class="bg-green-600 hover:bg-green-600 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow">
                         Log In
