556926d39b3f7a2433627340538ce431b4fad7e0
A Igandan <ai189@student.le.ac.uk>
2023-02-04 06:43:41 +0000

    Used tailwind (CDN) to create a better looking login page
    1. Tailwind CDN link added to the template file.
    2. Used tailwind to create a new login page
    3. Validation is maintained
    4. Works the same way bootstrap does- just different class names

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,6 +11,7 @@
     body {
         background-color: #2db28e;
     }
+
 </style>
 @endsection
 
@@ -19,39 +20,40 @@
     <div class="card shadow-lg" style="background-color: aliceblue">
         <img id="logo" src="{{ asset('assets/Full Logo/PNG/StokBox-Square-01.png') }}" width="150" alt="Logo" class="center">
         
-        <h1 class="center">Stokbox Platform</h1>
+        <h1 class="center">Stokbox Analytics</h1>
         <div style="padding: 50px;">
-        <form action="/processlogin" method="post">
-            @csrf
+            <form action="/processlogin" method="post" class="w-full max-w-sm">
+                @csrf
 
-            <div style="padding: 7px;">
-                <label for="username">Username </label>
-                <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" id="username" style="background-color: ">
-                @error('username')
-                <div class="invalid-feedback">
-                    {{ $message }}
+                <div class="flex items-center border-b border-teal-500 py-2">
+                    <input class="@error('username') is-invalid @enderror appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Username" name="username" id="username" aria-label="Username">
+                    @error('username')
+                    <br>
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
+                <br><br>
+                <div class="flex items-center border-b border-teal-500 py-2">
+                    <input class="@error('password') is-invalid @enderror appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Password" name="password" id="password" aria-label="Password">
+                    @error('password')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    <br><br>
                 @enderror
-
-                <br>
-            </div>
-            
-            <div style="padding: 7px;">
-                <label for="password">Password </label>
-                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password">
-                @error('password')
-                <div class="invalid-feedback">
-                    {{ $message }}
                 </div>
-                @enderror
+                
+                <div><a style="font-size:11px" href="{{ url('/user-manual') }}">Forgot your Password?</a></div>
+                <div>
+                    <button type="submit" style="background-color: #2db28e" class="bg-green-600 hover:bg-green-600 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow">
+                        Log In
+                      </button>
+                      <br><br>
+                    </div>
+              </form>
 
-                <br>
-            </div>
-            
-            <div style="padding: 7px;">
-            <input type="submit" value="Log in" class="btn btn-primary">
-            </div>
-        </form>
         </div>
     </div>
 </div>
