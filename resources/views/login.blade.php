4ba8557faeac327f1939d4dbebafe7b58d7b21f9
Jack Trafford <jt401@student.le.ac.uk>
2023-02-04 08:47:17 +0000

    Revert "Fixed password hiding and added show pswd funct"
    This reverts commit ecac824a60a3db32e7e0e10f4a3401ed9f5f7daa.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,9 +3,6 @@
 @section('title', 'Login')
 
 @section('head')
-
-
-
 <style>
     .center {
         margin: auto;
@@ -13,7 +10,6 @@
 
     body {
         background-color: #2db28e;
-        
     }
 
 
@@ -44,7 +40,7 @@
                 </div>
                 <br><br>
                 <div class="flex items-center border-b border-teal-500 py-2">
-                    <input class="@error('password') is-invalid @enderror appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="password" placeholder="Password" name="password" id="password" aria-label="Password">
+                    <input class="@error('password') is-invalid @enderror appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Password" name="password" id="password" aria-label="Password">
                     @error('password')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -52,24 +48,8 @@
                     <br><br>
                 @enderror
                 </div>
-                <!--Show Password functionality-->
-                <div>
-                    <a style="font-size:11px; padding-right:90px" href="{{ url('/user-manual') }}"><em>Forgot your Password?</em></a>
-
-                    <input style="" type="checkbox" onclick="myFunction()"> Show Password
-                    
-                    <script>
-                        function myFunction() {
-                            var x = document.getElementById("password");
-                            if (x.name === "password") {
-                              x.type = "text";
-                            } else {
-                              x.type = "password";
-                            }
-                          }
-                        </script>
-
-                </div>
+                
+                <div><a style="font-size:11px" href="{{ url('/user-manual') }}"><em>Forgot your Password?</em></a></div>
                 <div>
                     <button type="submit" style="background-color: #2db28e" class="bg-green-600 hover:bg-green-600 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow">
                         Log In
