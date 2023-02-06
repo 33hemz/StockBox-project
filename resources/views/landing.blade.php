5285e4345cf8d75569eac35d4a2f8f47ba290871
J Trafford <jt401@student.le.ac.uk>
2023-02-06 21:15:51 +0000

    Update landing page with responsive CSS

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,6 +1,6 @@
 @extends('templates.master')
 
-@section('title', 'StokBox home')
+@section('title', 'StokBox Home')
 
 @section('head')
 <style>
@@ -9,30 +9,28 @@
 @endsection
 
 @section('content') 
-<header id="landing_header">
-    <img id="landing_logo" src="{{ asset('assets/Full Logo/PNG/StokBox-02@3x.png') }}" alt="Logo">
-    <nav>
+<div class="d-flex flex-column h-100">
+    <header id="landing_header" class="d-flex align-items-center justify-content-between flex-column flex-md-row">
+        <img id="landing_logo" src="{{ asset('assets/Full Logo/PNG/StokBox-02@3x.png') }}" alt="StokBox Logo">
+        <nav>
         <ul>
-            <li><a href="#support">Support</a></li>
-            <li><a href="#about-us">About Us</a></li>
-            <li><a href="#features">Features</a></li>
-            <li id="login-button"><a href="{{ url('/login') }}" style="color:#2DB28E">Log In</a></li>
+            <li><a href="{{ url('/') }}">Home</a></li>
+            <li><a href="{{ url('/user-manual') }}">User Manual</a></li>
+            <li><a href="{{ url('/login') }}" class="text-primary">Log In</a></li>
         </ul>
-    </nav>
-</header>
+        </nav>
+    </header>
 
-<!--Hero Section Start-->
-<div class="hero__content hero container-custom position-relative ">
-    <div class="d-flex h-100 align-items-center hero__content-width">
-        <div id= "content" class="text-black">
-                <h1 class="fw-bold mb-4 ">Your products...<br>Our Insight</h1>
-                <p class="mb-4"><span class="green_embolden">Understand</span> and <span class="green_embolden"> visualise</span> the marketplace</p> 
-                <a href="{{ url('/login') }}" class="mt-2 col-12 btn btn-lg btn-outline-dark charm" style="background-color: #2DB28E; color:white; font-weight:bold" role="button">Get Started</a><br>
-                <a href="{{ url('/user-manual') }}" class="mt-2 btn btn-lg col-12 btn-outline-dark charm2" style="font-weight:500" role="button">Get Support</a>
-            
-        </div>
+    <div style="height: 15vh;"></div>
+
+    <div class="col-sm-12 col-md-3 offset-md-1">
+    <div class="text-black">
+        <h1 class="fw-bold mb-4 ">Your products...<br>Our Insight</h1>
+        <p class="mb-4 fs-5"><span class="text-primary fw-bold">Understand</span> and <span class="text-primary fw-bold">visualise</span> the marketplace</p> 
+        <a href="{{ url('/login') }}" class="btn btn-primary btn-lg mt-2 col-12 text-white fw-bold" role="button">Sign In</a><br>
+        <a href="{{ url('/user-manual') }}" class="btn btn-outline-secondary btn-lg mt-2 col-12" role="button">User Manual</a>
+    </div>
     </div>
 </div>
-<!--Hero Section End-->
 
 @endsection
