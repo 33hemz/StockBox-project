15b7f7a53b55bf0092570b634fb443259e996939
G K Rogers <gkr7@student.le.ac.uk>
2023-02-14 17:57:08 +0000

    Create 2023_02_14_173721_create_product_data_table.php

---
+++ database/migrations/2023_02_14_173721_create_product_data_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_data', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('brand');
+            $table->string('product_name');
+            $table->string('category');
+            $table->string('subcategory');
+            $table->double('price_£');
+            $table->string('price_per');
+            $table->string('ingredients');
+            $table->string('allergen information');
+            $table->string('recycling_information');
+            $table->string('link');
+            $table->string('brand_details');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_data');
+    }
+};
