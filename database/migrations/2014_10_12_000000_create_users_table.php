a2b815ab310ab4740e5747785fb6dc072ad54d9d
R Azam <ra444@student.le.ac.uk>
2023-02-15 11:39:01 +0000

    Update user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('company');
             $table->timestamps();
         });
     }
