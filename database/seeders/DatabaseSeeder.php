a673e43bdf715dcd7f043a9ae9e4324e501d32b1
D Wu <dw313@student.le.ac.uk>
2023-03-12 14:54:45 +0000

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,19 +32,19 @@ class DatabaseSeeder extends Seeder
         //     ['product_category_name' => 'Christmas'],   
         // )->create();
 
-        // \App\Models\Company::factory()->create([
-        //     'company_name' => 'Nestle',
-        // ]);
+        \App\Models\Company::factory()->create([
+            'company_name' => 'Nestle',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'user_type' => 'USER',
-        //     'first_name' => 'Test',
-        //     'last_name' => 'User',
-        //     'company_id' => 1,
-        //     'email' => 'user',
-        //     'password' => password_hash('password', PASSWORD_DEFAULT),
-        //     'init_user' => 1
-        // ]);
+        \App\Models\User::factory()->create([
+            'user_type' => 'USER',
+            'first_name' => 'Test',
+            'last_name' => 'User',
+            'company_id' => 1,
+            'email' => 'user',
+            'password' => password_hash('password', PASSWORD_DEFAULT),
+            'init_user' => 1
+        ]);
 
         \App\Models\User::factory()->create([
             'user_type' => 'ADMIN',
