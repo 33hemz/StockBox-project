fec90a40669d966a5f6311523c781653cdd1b388
D Wu <dw313@student.le.ac.uk>
2023-03-23 18:51:00 +0000

    Update to save state in local storage

--- public/themetoggle.js
+++ public/themetoggle.js
@@ -1,4 +1,8 @@
 const toggleButton = document.getElementById('dark-mode-toggle');
+const html = document.getElementsByTagName('html')[0];
+
+const currentTheme = localStorage.getItem('theme') || 'light';
+html.setAttribute('data-bs-theme', currentTheme);
 
     toggleButton.addEventListener('click', () => {
         
@@ -6,6 +10,7 @@ const toggleButton = document.getElementById('dark-mode-toggle');
         const currentTheme = html.getAttribute('data-bs-theme');
         const newTheme = currentTheme === 'light' ? 'dark' : 'light';
         html.setAttribute('data-bs-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
         
         // toggleButton.classList.toggle('bi-moon-stars-fill');
         // toggleButton.classList.toggle('bi-brightness-high-fill');
