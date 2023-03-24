8f79649566a01fb3b04dc4b60323225ab2ef6f66
T M P Angodage <tmpa1@student.le.ac.uk>
2023-03-24 20:44:17 +0000

    Change dashboard layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,37 +20,100 @@
 
 @section('content') 
 <h1 class="border-bottom pb-2 mb-3">Dashboard</h1>
-
-<h2>Demographic Data</h2>
 <div class="sec row row-cols-auto justify-content-evenly mb-5">
-    <div class="col card mx-2 my-3">
-        <h3 class="card-title fs-5">Gender Distribution</h3>
-        <div class="card-content"><canvas id="genderChart" style="min-height: 200px; max-height: 200px;"></canvas></div>
-    </div>
-    <div class="col card">
-        <h3 class="card-title fs-5">Age Distribution</h3>
-        <div class="card-content"><canvas id="ageChart" style="min-height: 200px; max-height: 200px;"></canvas></div>
-    </div>
-    <div class="col card mx-2 my-3">
-        <h3 class="card-title fs-5">Countries</h3>
-        <div class="card-content"><div id="countriesChart" style="min-width: 590px; max-width: 900px;"></div></div>
-    </div>
-    <div class="col card mx-2 my-3">
-        <h3 class="card-title fs-5">Average Income</h3>
-        <div class="card-content"><canvas id="incomeChart" style="min-height: 200px; max-height: 200px;"></canvas></div>
-    </div>
-    <div class="col card mx-2 my-3">
-        <h3 class="card-title fs-5">Number of Dependants</h3>
-        <div class="card-content"><canvas id="numOfDependentsChart" style="min-height: 200px; max-height: 200px;"></canvas></div>
-    </div>
+    <form method="GET" action="{{ route('dashboard') }}">
+        <div class="d-flex gap-3">
+            <div class="form-group">
+                <label for="gender">Gender:</label>
+                <select name="gender"  class="form-select">
+                    <option value=""></option>
+                    @foreach ((array_keys($genderData)) as $gender)
+                        <option value="{{ $gender }}">{{$gender}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="age">Age:</label>
+                <select class="form-select">
+                    <option value=""></option>
+                    @foreach ((array_keys($ageData)) as $age)
+                        <option value="{{ $age }}">{{$age}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="country">Countries:</label>
+                <select class="form-select">
+                    <option value=""></option>
+                    @foreach (($countries) as $country)
+                        <option value="{{ $country }}">{{$country}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="income">Income:</label>
+                <select class="form-select">
+                    <option value=""></option>
+                    @foreach ((array_keys($incomeData)) as $income)
+                        <option value="{{ $income }}">{{$income}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="number_of_dependants">Dependants:</label>
+                <select class="form-select">
+                    <option value=""></option>
+                    @foreach (($numOfDependentsData->keys()) as $dependants)
+                        <option value="{{ $dependants }}">{{$dependants}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="dietary">Dietary Requirements:</label>
+                <select class="form-select">
+                    <option value=""></option>
+                    @foreach (($dietaryData->keys()) as $dietary)
+                        <option value="{{ $dietary }}">{{$dietary}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Filter</button>
+        </div>
+    </form>    
 </div>
-
-<h2>Psychographic Data</h2>
-<div class="sec row row-cols-auto justify-content-evenly mb-5">
-    <div class="col card mx-2 my-3">
-        <h3 class="card-title fs-5">Dietary Requirements</h3>
-        <div class="card-content"><canvas id="dietaryChart" style="min-height: 200px; max-height: 200px;"></canvas></div>
+<div class="sec row">
+    <div class="row">
+        <div class="col card mx-2 my-3">
+            <h3 class="card-title fs-5">Gender Distribution</h3>
+            <div class="card-content"><canvas id="genderChart" style="min-height: 200px; max-height: 200px;"></canvas></div>
+        </div>
+        <div class="col card mx-2 my-3">
+            <h3 class="card-title fs-5">Number of Dependants</h3>
+            <div class="card-content"><canvas id="numOfDependentsChart" style="min-height: 200px; max-height: 200px;"></canvas></div>
+        </div>
     </div>
+    <div class="row">
+        <div class="col card mx-2 my-3">
+            <h3 class="card-title fs-5">Countries</h3>
+            <div class="card-content"><div id="countriesChart" style="min-width: 590px; max-width: 900px;"></div></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col card mx-2 my-3">
+            <h3 class="card-title fs-5">Average Income</h3>
+            <div class="card-content"><canvas id="incomeChart" style="min-height: 200px; max-height: 200px;"></canvas></div>
+        </div>
+        <div class="col card">
+            <h3 class="card-title fs-5">Age Distribution</h3>
+            <div class="card-content"><canvas id="ageChart" style="min-height: 200px; max-height: 200px;"></canvas></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col card mx-2 my-3">
+            <h3 class="card-title fs-5">Dietary Requirements</h3>
+            <div class="card-content"><canvas id="dietaryChart" style="min-height: 200px; max-height: 200px;"></canvas></div>
+        </div>
+    </div> 
 </div>
 
 <script>
