49189955944ab86f310378333959a68ee1472fd4
S S Cheema <ssc26@student.le.ac.uk>
2023-03-28 19:47:49 +0100

    Add mobile responsiveness to auth pages

--- resources/views/forgot_password/enter_new_password.blade.php
+++ resources/views/forgot_password/enter_new_password.blade.php
@@ -2,13 +2,8 @@
 
 @section('title', 'Enter New Password')
 
-@section('content') 
-<div class="mx-auto" style="width: 450px; margin-top: 50px;">
-    <div class="card shadow-lg bg-body-secondary">
-        <a href="{{ route('landing') }}" class="d-block mx-auto">
-            <img id="logo" src="{{ asset('assets/Full Logo/PNG/StokBox-Square-01.png') }}" width="150" alt="Logo" class="mx-auto">
-        </a>
-        <h1 class="text-center">Reset Password</h1>
+@section('content')
+        <h1 class="text-center mx-5">Reset Password</h1>
         
         <div class="mx-5 my-5 px-3">
             <form action="{{ route('password.update') }}" method="post">
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,40 +2,42 @@
 
 @section('title', 'Login')
 
-@section('content') 
-<div class="mx-auto position-relative" style="width: 450px; margin-top: 80px;">
-    <div class="card shadow-lg bg-body-secondary">
-        <a href="{{ route('landing') }}" class="d-block mx-auto">
-            <img id="logo" src="{{ asset('assets/Full Logo/PNG/StokBox-Square-01.png') }}" width="150" alt="Logo" class="mx-auto">
-        </a>
-        
-        <h1 class="text-center">StokBox Analytics</h1>
-        <div class="mx-5 my-5 px-3">
-            <form action="{{ route('process_login') }}" method="post">
-                @csrf
-                
-                <div class="mb-4">
-                    <label for="email">Email</label>
-                    <input type="text" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" name="email" id="email" autofocus>
-                    @error('email')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                
-                <div class="mb-4">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password">
-                    @error('password')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                
-                <div class="d-flex justify-content-between align-items-center">
-                    <input type="submit" value="Log in" class="btn btn-primary">
-                    <a href="{{ route('password.request') }}" class="font-italic text-primary">Forgot Password?</a>
-                </div>
-            </form>
-        </div>
+@section('content')       
+      <h1 class="text-center mx-5">StokBox Analytics</h1>
+      <div class="mx-5 my-5 px-3">
+        <form action="{{ route('process_login') }}" method="post">
+          @csrf
+          
+          <div class="mb-4">
+            <label for="email">Email</label>
+            <input type="text" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" name="email" id="email" autofocus>
+            @error('email')
+            <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+          </div>
+          
+          <div class="mb-4">
+            <label for="password">Password</label>
+            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password">
+            @error('password')
+            <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+          </div>
+          
+          <div class="d-flex justify-content-between align-items-center">
+            <div>
+              <input type="submit" value="Log in" class="btn btn-primary login-button">
+            </div>
+            <div class="text-end">
+              <a href="{{ route('password.request') }}" class="font-italic text-primary text-start">Forgot Password?</a>
+            </div>
+          </div>
+          
+          
+          
+        </form>
+      </div>
     </div>
+  </div>
 </div>
-@endsection
+  @endsection
